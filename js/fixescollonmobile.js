bb456969eeb4c8cc246c3e9bbb2e25b8ab4fd215
AMINE ISLAH <amineislah1997@gmail.com>
2020-07-27 21:31:29 +0100

    UPDATED

---
+++ js/fixescollonmobile.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    'use strict';
+    AOS.init({
+        duration: 1500,
+        once: true
+    });
+});
---
+++ js/script.js
@@ -0,0 +1,66 @@
+/*global $, document, check, window*/
+$(document).ready(function () {
+    'use strict';
+    $('.carousel').carousel({
+        interval: 4000
+    });
+});
+$(window).scroll(function () {
+    'use strict';
+    var height = $(window).scrollTop();
+    if (height >= 545) {
+        $(".navbar-inverse .navbar-nav > .active > a, .navbar-inverse .navbar-nav > .active > a:hover, .navbar-inverse .navbar-nav > .active > a:focus").css("background-color", "#ed143d");
+        $('.navbar-inverse').css("background-color", "#222");
+    } else {
+        $(".navbar-inverse .navbar-nav > .active > a, .navbar-inverse .navbar-nav > .active > a:hover, .navbar-inverse .navbar-nav > .active > a:focus").removeAttr("style");
+        $('.navbar-inverse').removeAttr("style");
+    }
+    if (height >= 1662) {
+        $(".up").css("display", "flex");
+    } else {
+        $(".up").hide();
+    }
+});
+
+function check() { // eslint-disable-line no-unused-vars
+    'use strict';
+    if ($('.navbar-toggle').hasClass("collapsed") === true) {
+        $(".collapse-padding").css("transition", "padding 0.5s ease");
+        $(".collapse-padding").mouseover(function () {
+            $(this).css("padding-left", "30px");
+        });
+        $(".collapse-padding").mouseout(function () {
+            $(this).css("padding-left", "15px");
+        });
+    } else {
+        $(".collapse-padding").removeAttr("style");
+        $(".collapse-padding").mouseover(function () {
+            $(this).css("padding-left", "15px");
+        });
+        $(".collapse-padding").mouseout(function () {
+            $(this).css("padding-left", "15px");
+        });
+    }
+}
+
+function goup() { // eslint-disable-line no-unused-vars
+    'use strict';
+    $("html,body").animate({
+        scrollTop: 0
+    }, 600);
+}
+$(window).on("load", function () {
+    'use strict';
+ /*   setInterval(function () {
+        $(".loading").fadeOut("slow",
+                    function () {
+                $(this).remove();
+                $(window).scrollTop(0);
+            }, 400);
+    });*/
+    setInterval(function(){
+              $("body").css("overflow","auto");
+               $(".loading").fadeOut("slow",function () {
+                $(this).remove();
+                $(window).scrollTop(0); });}, 2000);
+});
